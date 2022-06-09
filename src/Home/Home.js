c2bb94fa86c1330ae9b4390c2af0720d93fe8735
Mohuya748 <khandakermohuya@gmail.com>
2022-06-09 21:05:42 +0600

    projects added

--- src/Home/Home.js
+++ src/Home/Home.js
@@ -1,10 +1,14 @@
 import React from 'react';
 import About from './About';
+import Projects from './Projects';
+import Services from './Services';
 
 const Home = () => {
     return (
         <div>
             <About></About>
+            <Services></Services>
+            <Projects></Projects>
         </div>
     );
 };
--- src/Home/Services.js
+++ src/Home/Services.js
@@ -1,15 +1,12 @@
 import React from 'react';
-import project1 from '../images/project1.PNG'
-import project2 from '../images/project2.PNG'
-import project3 from '../images/project3.PNG'
 
-const Projects = () => {
+const Services = () => {
     return (
         <div className='container'>
-            <h2 className='text-center text-primary text-5xl font-mono font-bold '>My Projects</h2>
+            <h2 className='text-center text-primary text-5xl font-mono font-bold '>My Services</h2>
             <div className='m-10 grid lg:grid-cols-3 md:grid-cols-1 sm:grid-cols-1 gap-4'>
                 <div class="card w-96 bg-base-100 shadow-xl">
-                    <figure><img src={project1} alt="Shoes" /></figure>
+                    <figure><img src="https://api.lorem.space/image/shoes?w=400&h=225" alt="Shoes" /></figure>
                     <div class="card-body">
                         <h2 class="card-title">
                             Shoes!
@@ -23,7 +20,7 @@ const Projects = () => {
                     </div>
                 </div>
                 <div class="card w-96 bg-base-100 shadow-xl">
-                    <figure><img src={project2} alt="Shoes" /></figure>
+                    <figure><img src="https://api.lorem.space/image/shoes?w=400&h=225" alt="Shoes" /></figure>
                     <div class="card-body">
                         <h2 class="card-title">
                             Shoes!
@@ -37,7 +34,7 @@ const Projects = () => {
                     </div>
                 </div>
                 <div class="card w-96 bg-base-100 shadow-xl">
-                    <figure><img src={project3} alt="Shoes" /></figure>
+                    <figure><img src="https://api.lorem.space/image/shoes?w=400&h=225" alt="Shoes" /></figure>
                     <div class="card-body">
                         <h2 class="card-title">
                             Shoes!
@@ -55,4 +52,4 @@ const Projects = () => {
     );
 };
 
-export default Projects;
+export default Services;
